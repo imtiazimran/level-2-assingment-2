d3374d7ab68f705cf85aa0a341c692591a9c6129
imtiazimran <imtiazimran@outlook.com>
2023-11-23 01:49:27 +0600

    user model created

---
+++ src/app/module/users/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+import { TAddress, TUser, TUserName } from "./user.interface";
+
+
+const nameSchema = new Schema<TUserName>({
+    firstName: String,
+    lastName: String
+})
+
+const addressShema = new Schema<TAddress>({
+    street: String,
+    city: String,
+    country: String
+})
+
+const userSchema = new Schema<TUser>({
+    userId: {type: Number, required: true},
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    fullName: nameSchema,
+    age: {type: Number, required: true},
+    email: {type: String, required: true},
+    isActive: {type: Boolean, default: true},
+    hobbies: [String],
+    address: addressShema
+})
+
+
+const UserModel = mongoose.model("user", userSchema)
+
+
